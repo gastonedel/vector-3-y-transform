45cd655f3b17cb1c045d1e6bcbfd314091c42459
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-17 10:13:42 -0300

    .

---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour {
+    public Vector3 miVector;
+    public GameObject myObject;
+	// Use this for initialization
+	void Start () {
+        myObject.transform.position = miVector;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
